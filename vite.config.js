85c934aaa16e5b33041dc4c6d7604849847fe874
Gerard Eklu <gerard.eklu@yahoo.fr>
2025-03-21 18:58:25 -0400

    Initial commit - Initialized a React App and set up tailwind css config

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+});
