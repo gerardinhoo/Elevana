45c2903e871ca62fc85355727eb1ec663c24d162
Gerard Eklu <gerard.eklu@yahoo.fr>
2025-03-25 14:23:23 -0400

    Worked on adding the features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './sections/Header';
 import Hero from './sections/Hero';
+import Features from './sections/Features';
 
 const App = () => {
   return (
     <main>
       <Header />
       <Hero />
+      <Features />
     </main>
   );
 };
